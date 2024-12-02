e22fda4c9f03163787d423d7c9af44912e8b9aea
admor <adrien_morvan@hotmail.fr>
2024-12-02 23:01:55 +0100

    Correct the langfuse call

--- observability/workflow.py
+++ observability/workflow.py
@@ -1,21 +1,52 @@
 import os
 import asyncio
-from llama_index.core.workflow import Workflow, StartEvent, StopEvent, step
+from langfuse.decorators import observe
 from llama_index.core import set_global_handler, global_handler
+import os
+from pydantic import BaseModel, Field
+from llama_index.core.workflow import (
+    Workflow,
+    step,
+    Event,
+    Context,
+    StartEvent,
+    StopEvent
+)
+from llama_index.llms.openai import OpenAI
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import (
+    SimpleDirectoryReader,
+)
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.retrievers.bm25 import BM25Retriever
+import Stemmer
 
-os.environ["LANGFUSE_SECRET_KEY"] = "sk-lf-cff8c32a-05d7-4ca7-924b-592ab992b8ab"
-os.environ["LANGFUSE_PUBLIC_KEY"] = "pk-lf-b97a846a-eb65-450a-8c14-bb95070676e8"
+
+os.environ["LANGFUSE_SECRET_KEY"] = "sk-lf--05d7--924b-592ab992b8ab"
+os.environ["LANGFUSE_PUBLIC_KEY"] = "pk-lf--eb65--8c14-bb95070676e8"
 os.environ["LANGFUSE_HOST"] = "http://localhost:3000"
 set_global_handler("langfuse")
 
 class EchoWorkflow(Workflow):
     """A dummy workflow with only one step sending back the input given."""
 
+    @observe()
     @step()
     async def run_step(self, ev: StartEvent) -> StopEvent:
         message = str(ev.get("message", ""))
-        print("Coucouc")
-        return StopEvent(result=f"Message received: {message}")
+        documents = SimpleDirectoryReader(
+            input_files=["../notebooks/paul_graham_essay.txt"],
+        ).load_data()
+        splitter = SentenceSplitter(chunk_size=256)
+        nodes = splitter.get_nodes_from_documents(documents)
+        retriever_top_5 = BM25Retriever.from_defaults(
+            nodes=nodes,
+            similarity_top_k=5,
+            stemmer=Stemmer.Stemmer("english"),
+            language="english",
+        )
+        nodes = retriever_top_5.retrieve(message)
+        return StopEvent(result=f"Message received: {nodes[0].text}")
 
 
 # `echo_workflow` will be imported by Llama Deploy
